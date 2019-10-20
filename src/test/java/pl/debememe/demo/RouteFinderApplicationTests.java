d0d9dc37dbc69a7a56bad26e32808e02f7122195
bajserowiczMikolaj <mbajserowicz@gmail.com>
2019-10-20 11:27:31 +0100

    001

---
+++ src/test/java/pl/debememe/demo/RouteFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.debememe.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
