0727b1e76faf1948e958b7679e74219189273dc2
bajserowiczMikolaj <mbajserowicz@gmail.com>
2019-10-20 11:57:37 +0100

    002

--- src/main/java/pl/debememe/demo/RouteFinderApplication.java
+++ src/main/java/pl/debememe/demo/RouteFinderApplication.java
@@ -8,6 +8,9 @@ public class RouteFinderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RouteFinderApplication.class, args);
+
+
+        System.out.println("test");
     }
 
 }
