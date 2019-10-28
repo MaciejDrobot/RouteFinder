072f01786ac052782cdf43bb753d5944c124b6fb
Maciej Drobot <maciejdrobot@gmail.com>
2019-10-28 21:52:50 +0000

    home button, center: London, directions trough navber working

--- src/main/java/pl/debememe/demo/maps/MapsController.java
+++ src/main/java/pl/debememe/demo/maps/MapsController.java
@@ -10,14 +10,11 @@ public class MapsController {
 
     MapsProvider mapsProvider;
 
-
     @GetMapping
     @RequestMapping("/home")
     public String showHomePage(Model model){
-        String latitude = "58.400";
-        String longitude = "22.010";
-        model.addAttribute("latitude", latitude);
-        model.addAttribute("longitude", longitude);
+        model.addAttribute("latitude", "51.509865");
+        model.addAttribute("longitude", "-0.118092");
         model.addAttribute("route", new Route());
         return "home";
     }
@@ -25,8 +22,10 @@ public class MapsController {
     @PostMapping
     @RequestMapping("/showRoute")
     public String showRoute(@ModelAttribute Route route, Model model){
+
         model.addAttribute("start", route.getStart());
         model.addAttribute("end", route.getEnd());
+
         return "directions";
     }
 
